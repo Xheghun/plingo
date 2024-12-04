956b19a44c37b418e7435eeae28350febbf863c5
David (Oluwasegun) Rufai <ysenruf@gmail.com>
2024-12-04 18:52:53 +0100

    start game and handle key pressed events

--- lib/presentation/bloc/game_bloc.dart
+++ lib/presentation/bloc/game_bloc.dart
@@ -1,30 +1,3 @@
-/// Copyright (c) 2022 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to
-/// deal in the Software without restriction, including without limitation the
-/// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-/// sell copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge,
-/// publish, distribute, sublicense, create a derivative work, and/or sell
-/// copies of the Software in any work that is designed, intended, or marketed
-/// for pedagogical or instructional purposes related to programming, coding,
-/// application development, or information technology.  Permission for such
-/// use, copying, modification, merger, publication, distribution, sublicensing,
-///  creation of derivative works, or sale is expressly withheld.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-///  IN THE SOFTWARE.
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
@@ -32,17 +5,32 @@ import '../../data.dart';
 import '../../domain.dart';
 
 part 'game_event.dart';
+
 part 'game_state.dart';
 
 /// Handles all logic related to the game.
 class GameBloc extends Bloc<GameEvent, GameState> {
   /// Constructor
-  GameBloc(this._statsRepository) : super(GameState(guesses: emptyGuesses()));
+  GameBloc(this._statsRepository) : super(GameState(guesses: emptyGuesses())) {
+    on<GameStarted>(_onGameStarted);
+    on<LetterKeyPressed>(_onLetterKeyPressed);
+  }
 
   /// Interacts with storage for updating game stats.
   final GameStatsRepository _statsRepository;
 
-  // TODO: Add logic for GameStarted
+  void _onGameStarted(GameEvent event, Emitter<GameState> emit) {
+    print('Game has started!');
+    final puzzle = nextPuzzle(puzzles);
+    final guesses = emptyGuesses();
+
+    emit(GameState(guesses: guesses, puzzle: puzzle));
+  }
+
   // TODO: Add logic for GameFinished
-  // TODO: Add logic for LetterKeyPressed
+  void _onLetterKeyPressed(LetterKeyPressed event, Emitter<GameState> emit) async{
+    final guesses = addLetterToGuesses(state.guesses, event.letter);
+
+    emit(state.copyWith(guesses: guesses));
+  }
 }
