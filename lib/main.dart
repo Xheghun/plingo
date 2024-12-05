7d6620592595a4bbaef1192a0bed1799ea0813c0
David (Oluwasegun) Rufai <ysenruf@gmail.com>
2024-12-05 09:30:20 +0100

    add a bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'app/app.dart';
 import 'data.dart';
+import 'monitoring/bloc_monitor.dart';
 
 void main() {
-  runApp(RepositoryProvider(
-    create: (context) => GameStatsRepository(GameStatsSharedPrefProvider()),
-    child: const PlingoApp(),
-  ));
+  BlocOverrides.runZoned(() => runApp(RepositoryProvider(
+        create: (context) => GameStatsRepository(GameStatsSharedPrefProvider()),
+        child: const PlingoApp(),
+      )), blocObserver: BlocMonitor());
 }
--- lib/monitoring/bloc_monitor.dart
+++ lib/monitoring/bloc_monitor.dart
@@ -1,1 +1,14 @@
 
+import 'package:flutter/foundation.dart' as foundation;
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+///[BlockObserver] for the application which
+///observer all state changes.
+class BlocMonitor extends BlocObserver {
+
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    foundation.debugPrint('${bloc.runtimeType} $change');
+  }
+}
