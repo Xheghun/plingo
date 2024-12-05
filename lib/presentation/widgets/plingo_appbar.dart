6bff8a28f7449cdcaf1967c7b3bdae60f4c382c4
David (Oluwasegun) Rufai <ysenruf@gmail.com>
2024-12-05 09:19:57 +0100

    add reset feature

--- lib/presentation/widgets/plingo_appbar.dart
+++ lib/presentation/widgets/plingo_appbar.dart
@@ -32,9 +32,8 @@ class PlingoAppBar extends StatelessWidget implements PreferredSizeWidget {
             context: context,
             builder: (dContext) => BlocProvider(
               create: (bContext) => StatsCubit(
-                // TODO: Fetch the stats to display them.
                 context.read<GameStatsRepository>(),
-              ),
+              )..fetchStats(),
               child: const GameStatsDialog(),
             ),
           ),
